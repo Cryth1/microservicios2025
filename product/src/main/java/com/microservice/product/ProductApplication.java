d968de347b48dcc2dc2534a064c65a9a61c25444
crist <cristianguerrero06@hotmail.com>
2025-03-13 11:58:45 -0500

    second commint

--- product/src/main/java/com/microservice/product/ProductApplication.java
+++ product/src/main/java/com/microservice/product/ProductApplication.java
@@ -2,9 +2,11 @@ package com.microservice.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableReactiveMongoRepositories(basePackages = "com.microservice.product.repository")
 public class ProductApplication {
 

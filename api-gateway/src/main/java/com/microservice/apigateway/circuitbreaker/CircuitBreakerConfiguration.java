2354165c05ddd1d35d4e3b8aa9a1b2f899e59dee
crist <cristianguerrero06@hotmail.com>
2025-03-30 00:52:34 -0500

    Api Gateway implementado

--- api-gateway/src/main/java/com/microservice/apigateway/circuitbreaker/CircuitBreakerConfiguration.java
+++ api-gateway/src/main/java/com/microservice/apigateway/circuitbreaker/CircuitBreakerConfiguration.java
@@ -1,7 +1,8 @@
 package com.microservice.apigateway.circuitbreaker;
 
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
 import java.time.Duration;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
@@ -13,13 +14,16 @@ public class CircuitBreakerConfiguration {
 
     @Bean
     public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
-        CircuitBreakerConfig config = CircuitBreakerConfig.custom()
-                .failureRateThreshold(50) // % de fallos para abrir el circuito
-                .waitDurationInOpenState(Duration.ofSeconds(10)) // Tiempo en estado abierto
-                .slidingWindowSize(5) // Número de llamadas para calcular el umbral
-                .build();
+        return factory -> factory.configureDefault(id -> {
+            CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+                    .failureRateThreshold(50) // Umbral de fallo del 50%
+                    .waitDurationInOpenState(Duration.ofSeconds(10)) // Tiempo en estado abierto
+                    .slidingWindowSize(5) // Tamaño de la ventana para calcular el umbral
+                    .build();
 
-        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder.Resilience4JCircuitBreakerConfiguration());
+            return new Resilience4JConfigBuilder(id)
+                    .circuitBreakerConfig(circuitBreakerConfig)
+                    .build();
+        });
     }
-
 }
